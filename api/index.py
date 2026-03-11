4b5d45df588d89999c5e540752475408e6c8c504
Ishwari <ishwaritidke24@gmail.com>
2026-03-11 20:09:22 +0530

    build(api): add ProxyFix to vercel api/index.py to handle reverse proxy and URL paths

--- api/index.py
+++ api/index.py
@@ -1,1 +1,6 @@
 from app import app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+# Vercel relies on reverse proxy headers. We need to tell Flask to trust them
+# so it correctly routes /api/... requests
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
